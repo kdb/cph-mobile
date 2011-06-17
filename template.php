abcd4f9d13157d654afb09e7de4a33ca12f54179
Jesper Woeldiche <jesper@woeldiche.dk>
2011-06-17 11:54:09 +0200

    Added screenshot, more basic styling, backgrounds

--- template.php
+++ template.php
@@ -2,7 +2,9 @@
 /**
  * Implementation of hook_preprocess_page.
  */ 
-function aarhus_mobile_preprocess_page(&$vars) {
+function cph_mobile_preprocess_page(&$vars) {
+  print_r($vars);
+  
   // Don't display empty help from node_help().
   if ($vars['help'] == "<div class=\"help\"> \n</div>") {
     $vars['help'] = '';
@@ -15,4 +17,37 @@ function aarhus_mobile_preprocess_page(&$vars) {
     $vars['site_logo'] = '<a class="site-logo" href="'. $vars['front_page'] .'" title="'. t('Home page') .'" rel="home"><img src="'. $vars['logo'] .'" alt="'. $vars['logo_alt_text'] .'" /></a>';
   }   
 
+}
+
+/**
+ * Implementation of hook_theme.
+ */
+function cph_mobile_theme() {
+  return array(
+    'user_login' => array(
+      'arguments' => array('form' => NULL),
+    ),
+    'ting_search_form' => array(
+    'arguments' => array('form' => NULL),
+    ),
+  );
+}
+
+/**
+ * Theme function used to change the login box.
+ *
+ * Alternator hiddes some information, that we do not... so we overwrite it here.
+ */
+function cph_mobile_user_login($form) {
+  return drupal_render($form);
+}
+
+/**
+ * Theme function that can be used to remove stuff form the search form. The
+ * h2 headline can be disabled on the block for current theme.
+ */
+function cph_mobile_ting_search_form(&$form){
+  print_r($form);
+  unset($form['example_text']);
+  return drupal_render($form);
 }
